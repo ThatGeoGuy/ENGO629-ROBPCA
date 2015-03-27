6afab5459dbb736db0f93da25377efa0356b013f
Jeremy Steward <stewardj@ucalgary.ca>
2015-03-27 15:22:44 -0600

    Adds method to compute # of least-outlying points

--- engo629/robust_pca.py
+++ engo629/robust_pca.py
@@ -16,7 +16,7 @@ class ROBPCA(object):
     Rousseeuw, and Karlien Vandem Branden (2005)
     """
 
-    def __init__(self, X, kmax=10):
+    def __init__(self, X, kmax=10, alpha=0.75):
         """
         Initializes the class instance with the data you wish to compute the
         ROBPCA algorithm over.
@@ -29,24 +29,26 @@ class ROBPCA(object):
 
         kmax : Maximal number of components that will be computed. Set to 10
                by default
+
+        alpha : Assists in determining step 2. The higher alpha is, the more
+                efficient the estimates will be for uncontaminated data.
+                However, lower values for alpha make the algorithm more robust.
+                Can be any real value in the range [0.5, 1].
         """
-        self.data = X
-        self.kmax = kmax
+        if not (0.5 <= alpha <= 1.0):
+            raise ValueError("ROBPCA: alpha must be a value in the range [0.5, 1.0]")
+
+        self.data  = X
+        self.kmax  = kmax
+        self.alpha = alpha
         return
 
-    @staticmethod
-    def reduce_to_affine_subspace(data):
+    def reduce_to_affine_subspace(self):
         """
         Takes the mean-centred data-matrix and computes the affine subspace
         spanned by n observations. That is, we take the singular value
         decomposition of the mean-centred data-matrix and use Z = UD as our
         new data matrix where Z is an n by r0 sized matrix.
-
-        Arguments:
-        ----------
-
-        data : An n x p data matrix (where n is number of data points and p is
-               number of dimensions in data) which is to be reduced.
 
         Returns:
         --------
@@ -54,6 +56,25 @@ class ROBPCA(object):
         Z : Z is the product of U and D of the singular value decomposition of
             the mean-centred data matrix
         """
-        centred_data = data - np.mean(self.data, axis=0)
+        centred_data = self.data - np.mean(self.data, axis=0)
         U, D, V = np.linalg.svd(centred_data, False)
         return U * D
+
+    def num_least_outlying_points(self):
+        """
+        Determines the least number of outlying points h, which should be less
+        than n, our number of data points. `h` is computed as the maximum of either:
+
+            alpha * n           OR
+            (n + kmax + 1) / 2
+
+        Returns:
+        --------
+
+        h : number of least outlying points.
+        """
+        n = self.data.shape[0]
+        return np.max([
+            self.alpha * n,
+            (n + self.kmax + 1) / 2,
+            ])
