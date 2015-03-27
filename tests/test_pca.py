e955174ccb3c8e610c218b63b513c8997ce7db5e
Jeremy Steward <stewardj@ucalgary.ca>
2015-03-27 13:11:40 -0600

    Test passes -> independent of constants (to some extent)

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -7,6 +7,8 @@
 Tests functionality in robpca/pca.py
 """
 import unittest
+from random import randint
+
 import numpy as np
 
 from robpca import pca
@@ -16,27 +18,48 @@ class TestClassicalPCA(unittest.TestCase):
     Tests function to compute classical principal components analysis.
     """
 
-    def test_normal_to_z_plane(self):
+    def test_normal_to_cardinal_plane(self):
         """
-        Tests whether computing PCA over a point cloud distributed in the plane
-        Z = 0 provides the unit vector [0, 0, 1].
+        Tests whether computing PCA over a set of data with one dimension set
+        to constant yields a principal component of unit length with all
+        dimension values set to zero except that of the dimension which is
+        constant in the data matrix.
+
+        e.g. if we have an XYZ point cloud and all Z values are set to zero,
+        there is no variance in the Z direction therefore the PC corresponding
+        to the plane defined by the point cloud should be [0,0,1]
         """
 
+        # Data sizes and axis number (the axis that will be set to zero)
+        #
+        n    = randint(4, 1500)
+        p    = randint(2, 100)
+        axis = randint(0, p - 1)
+
         # Initialize data matrix
-        X       = np.zeros((1000, 3))
-        X[:, 0] = np.random.rand(1000)
-        X[:, 1] = np.random.rand(1000)
+        X          = np.random.rand(n, p)
+        X[:, axis] = 0
 
         # Calculate PCs and respective variances
         variances, PCs = pca.principal_components(X)
-
         min_var        = np.min(variances)
         pc_min_var     = PCs[:, np.argmin(variances)]
 
         # Expected outputs
-        expect_min_var = 0
-        expect_pc      = np.array([0,0,1])
+        expect_min_var  = 0
+        expect_pc       = np.zeros(p)
+        expect_pc[axis] = 1
 
-        self.assertEqual(min_var, expect_min_var)
-        self.assertTrue(np.all(pc_min_var == expect_pc))
+        # Machine precision and Tests
+        # When testing the actual principal component, note that it is
+        # necessary to test both the PC and it's complement (-1 * PC) because
+        # the direction of a unit vector is arbitrary (e.g. Z axis can point up
+        # or down, but it still defines the same axis)
+        eps = abs(np.spacing(1))
+
+        self.assertTrue(abs(min_var - expect_min_var) < eps)
+        self.assertTrue(
+            np.all(np.abs( pc_min_var - expect_pc) < 10 * eps) or
+            np.all(np.abs(-pc_min_var - expect_pc) < 10 * eps)
+        )
         return
