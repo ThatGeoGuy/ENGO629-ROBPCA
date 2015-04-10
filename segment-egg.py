05c6d757d3c3cffde812c4fd2b9711beb1f3aaad
Jeremy Steward <stewardj@ucalgary.ca>
2015-04-10 12:34:34 -0600

    Finished egg segmentation program

--- segment-egg.py
+++ segment-egg.py
@@ -11,12 +11,13 @@ import argparse
 import sys
 
 import numpy as np
-from scipy.spatial import KDTree
+# from scipy.spatial import KDTree
 from sklearn.covariance import MinCovDet
+from sklearn.neighbors import NearestNeighbors
 
 from engo629.classic_pca import principal_components
 
-def main(filename, alpha, tol):
+def main(filename, alpha, ttol, dtol, classic, ofile):
     """
     Main procedure. Runs a program to segment objects with dissimilar
     principal components from the robust PCA of the entire set of points.
@@ -36,43 +37,54 @@ def main(filename, alpha, tol):
         raise ValueError("segment-egg.py: The support fraction must be between 0.5 and 1")
 
     # Load data and insert it into a KDTree
-    kdt = KDTree(np.loadtxt(filename))
-    # Apply MCD method to full data
-    mcd = MinCovDet(support_fraction=alpha).fit(kdt.data)
+    data = np.loadtxt(filename)
+    kNN  = NearestNeighbors(n_neighbors = 8, p = 2).fit(data)
 
-    # PCs of full (global) dataset
-    gL, gPC = principal_components(None, lambda x: mcd.covariance_)
+    if classic:
+        loc     = np.mean(data, axis= 0)
+        gL, gPC = principal_components(data)
+    else:
+        # Apply MCD method to full data
+        mcd = MinCovDet(support_fraction=alpha).fit(data)
+        loc = mcd.location_
+
+        # PCs of full (global) dataset
+        gL, gPC = principal_components(None, lambda x: mcd.covariance_)
+
+    proj = abs(np.dot(loc, gPC[:,2]))
 
     print("Global planarity is:\n{}".format(gPC[:, 2]))
+    print("Global eigenvalues: {}".format(gL))
+    print("Location is: {}".format(loc))
 
-    marked_data        = np.zeros((kdt.data.shape[0], 5))
-    makred_data[:,0:3] = kdt.data.copy()
+    marked_data        = np.ones((data.shape[0], 4))
+    marked_data[:,0:3] = data.copy()
 
-    for i, pt in enumerate(kdt.data):
-        dists, nn_indices = kdt.query(pt, k=8, p=2)
+    for i, pt in enumerate(data):
+        dists, nn_indices = kNN.kneighbors(pt, n_neighbors=8)
 
-        LL, LPC = principal_components(kdt.data[nn_indices, :])
+        LL, LPC = principal_components(data[nn_indices[0], :])
 
         # Calculate angle between planar components, then convert
         # to degrees
-        theta = np.acos(np.dot(LPC[:, 2], gPC[:, 2]) /
-                (np.norm(LPC[:, 2]) * np.norm(gPC[:, 2])))
+        # A <dot> B = |A| * |B| * cos(theta)
+        theta = np.arccos(np.dot(LPC[:, 2], gPC[:, 2]) /
+                (np.linalg.norm(LPC[:, 2]) * np.linalg.norm(gPC[:, 2])))
         theta *= 180 / np.pi
 
+        loc  = abs(np.dot(pt, gPC[:,2]))
+        dist = abs(loc - proj)
+
         # Check that the normals aren't out of phase
-        if theta >= 180:
+        if theta > 90:
             theta -= 180
-        elif theta <= -180:
+        if theta < -90:
             theta += 180
 
-        if theta > tol:
+        if ((abs(theta) > ttol) and (LL[2] > gL[2])) or dist > dtol:
             marked_data[i, 3] = 0
-        else:
-            marked_data[i, 3] = 255
 
-        marked_data[i, 4] = theta
-
-    np.savetxt(sys.stdout, marked_data, '%.6f')
+    np.savetxt(ofile, marked_data, '%.6f')
     return 0
 
 if __name__ == '__main__':
@@ -86,11 +98,26 @@ if __name__ == '__main__':
             type=float,
             default=0.75,
             help='Support fraction (alpha) of robust PCA')
-    parser.add_argument('--tol',
-            metavar='TOLERANCE',
+    parser.add_argument('--ttol',
+            metavar='THETA TOL',
             type=int,
+            default=5,
             help='Tolerance between global and local plane angles in degrees')
+    parser.add_argument('--dtol',
+            metavar='DIST TOL',
+            type=float,
+            default=1e-2,
+            help='Location tolerance between point and background plane')
+    parser.add_argument('--classic',
+            action='store_true',
+            default=False,
+            help='Tells the algorithm to use robust PCA or classic (robust is default)')
+    parser.add_argument('-o',
+            metavar='OUTPUT',
+            type=str,
+            default='output.txt',
+            help='Name of the file you want to output the data to')
 
     args = parser.parse_args()
 
-    main(args.xyz_file, args.alpha, args.tol)
+    main(args.xyz_file, args.alpha, args.ttol, args.dtol, args.classic, args.o)
